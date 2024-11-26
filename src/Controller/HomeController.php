1e7aa5b89c3b055131320995ddb3a83e85786529
Florian Logez <florian@tool4staffing.com>
2024-11-26 14:07:34 +0100

    connect to db

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Service\T4SWrapper\T4SInteraction;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,25 +12,29 @@ class HomeController extends AbstractController {
 
     private $t4s;
 
-    public function __construct(T4SInteraction $t4s)
+    private $db;
+
+    public function __construct(T4SInteraction $t4s, ManagerRegistry $db)
     {
         $this->t4s = $t4s;
+        $this->db = $db;
     }
 
     #[Route('/')]
     public function index(): Response
     {
 
-        $data = [
-            'test1' => 'value1',
-            'test2' => 'value2',
-            'test3' => 'value3',
-        ];
+        $connection = $this->db->getConnection('account');
+        $result = $connection->fetchAllAssociative('SELECT * FROM agents WHERE trash = 0');
+
+        $connection = $this->db->getConnection('tool');
+        $accountInfos = $connection->fetchAllAssociative('SELECT * FROM accounts WHERE trash = 0 AND id_unique = "confstarte"');
 
         return $this->render('home/index.html.twig', [
             'homeMsg' => 'Welcome!',
             'test' => $this->t4s->test(),
-            'data' => $data,
+            'data' => $result,
+            'accountTitle' => $accountInfos[0]['title'],
         ]);
     }
 }
